e6c956c56b6fbde9ef51e7c3de758806cb0fc9c3
aditya-kumar23 <63554248+aditya-kumar23@users.noreply.github.com>
2023-11-20 23:21:43 +0100

    re throwing exception

--- capstone_project/src/capstone/ChargeStation.java
+++ capstone_project/src/capstone/ChargeStation.java
@@ -16,6 +16,27 @@ class listeningException extends Exception {
         super(message);
     }
 }
+class FileNotExistException extends Exception {
+    public FileNotExistException(String message) {
+        super(message);
+    }
+}
+class ChargingStationException extends Exception{
+	public ChargingStationException(String message) {
+		        super(message);
+	    	}
+}
+
+class NoAvailableChargingLocationException extends ChargingStationException{
+	public NoAvailableChargingLocationException(String message) {
+		        super(message);
+	    	}
+}
+class LongWaitingTimeException extends ChargingStationException{
+	public LongWaitingTimeException(String message) {
+		        super(message);
+	    	}
+}
 
 public class ChargeStation {
 	
@@ -38,35 +59,50 @@ public class ChargeStation {
 		/* 
 		 * return  id of waiting vehicle from requestCar and bookFile
 		 */
-		boolean file_exist = false;
-		// read booking file
-		if (file_exist) {
-			
-		}else {
-			throw new listeningException("could not found booking file");
-		}
-			
-		
-		// Logging
-		
-		return 0;
-	}
+		 /* 
+         * return  id of waiting vehicle from requestCar and bookFile
+         */
+        try {
+            boolean file_exist = false;
+            // read booking file
+            if (file_exist) {
+
+            } else {
+                throw new FileNotExistException("Booking file not found");
+            }
+
+            // Logging
+
+            return 0;
+
+        } catch (FileNotExistException e) {
+            // Log the exception or take appropriate actions locally
+            System.err.println("Error: " + e.getMessage());
+            // Re-throw the exception for higher-level handling
+            throw new listeningException("Error while listening for vehicle");
+        }
+    }
 	
 	void assigning(int carID, int chargerID) throws assigningException {
-		/* car id as the input
-		 * assign the vehicle to a specific charger
-		 */
-		int vehicle_id = -1;
-		// get vehicle id 
-		if (vehicle_id < 0) {
-			throw new assigningException("vehicle is not exist");
-		}
-		
-		
-		// Logging
-		
-	}
-	
+		 /* car id as the input
+         * assign the vehicle to a specific charger
+         */
+        try {
+            int vehicle_id = -1;
+            // get vehicle id 
+            if (vehicle_id < 0) {
+                throw new assigningException("Vehicle is not exist");
+            }
+
+            // Logging
+
+        } catch (assigningException e) {
+            // Log the exception or take appropriate actions locally
+            System.err.println("Error: " + e.getMessage());
+            // Re-throw the exception for higher-level handling
+            throw e;
+        }
+    }
 	void book(int id, double timeSlot) {
 		/* 
 		 * book a time slot by any car to charge
