20b5913e343a920677d719e623cc5952090f41bb
sheikh-adib <sheikh.binshabubakar001@stud.fh-dortmund.de>
2023-11-20 22:13:53 +0100

    update

--- capstone_project/src/capstone/ChargeStation.java
+++ capstone_project/src/capstone/ChargeStation.java
@@ -3,6 +3,19 @@ package capstone;
 import java.util.ArrayList;
 import java.io.IOException;
 
+// exceptions
+
+class assigningException extends Exception {
+	public assigningException(String message) {
+        super(message);
+    }
+}
+
+class listeningException extends Exception {
+	public listeningException(String message) {
+        super(message);
+    }
+}
 
 public class ChargeStation {
 	
@@ -21,20 +34,34 @@ public class ChargeStation {
 		return 0;
 	}
 	
-	int listening( String bookFilePath) {
+	int listening( String bookFilePath) throws listeningException{
 		/* 
 		 * return  id of waiting vehicle from requestCar and bookFile
 		 */
+		boolean file_exist = false;
+		// read booking file
+		if (file_exist) {
+			
+		}else {
+			throw new listeningException("could not found booking file");
+		}
+			
 		
 		// Logging
 		
 		return 0;
 	}
 	
-	void assigning(int carID, int chargerID) {
+	void assigning(int carID, int chargerID) throws assigningException {
 		/* car id as the input
 		 * assign the vehicle to a specific charger
 		 */
+		int vehicle_id = -1;
+		// get vehicle id 
+		if (vehicle_id < 0) {
+			throw new assigningException("vehicle is not exist");
+		}
+		
 		
 		// Logging
 		
@@ -49,19 +76,7 @@ public class ChargeStation {
 		
 	}
 	
-	// exceptions
-	
-	class assigningException extends Exception {
-		public assigningException(String message) {
-	        super(message);
-	    }
-	}
-	
-	class listeningException extends Exception {
-		public listeningException(String message) {
-	        super(message);
-	    }
-	}
+
 	
 	
 	// setters and getters
