72d56a55c8cda35071effbff1fdb5d4fb61a4305
sheikh-adib <sheikh.binshabubakar001@stud.fh-dortmund.de>
2023-11-20 17:01:57 +0100

    Update Charger.java

--- capstone_project/src/capstone/Charger.java
+++ capstone_project/src/capstone/Charger.java
@@ -5,4 +5,22 @@ public class Charger {
 	int id;
 	boolean occupied;
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public boolean isOccupied() {
+		return occupied;
+	}
+	public void setOccupied(boolean occupied) {
+		this.occupied = occupied;
+	}
+	
+	@Override
+	public String toString() {
+		return "Charger [id=" + id + ", occupied=" + occupied + "]";
+	}
+	
 }
