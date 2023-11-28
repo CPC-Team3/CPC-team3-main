d5ea4a68a17e6e56b943c1c76d7d4825b61acdc4
sheikh-adib <sheikh.binshabubakar001@stud.fh-dortmund.de>
2023-11-28 18:59:23 +0100

    update

--- capstone_project/src/charging_station/Charger.java
+++ capstone_project/src/charging_station/Charger.java
@@ -1,10 +1,45 @@
 package charging_station;
 
+import charging_station.ExceptionHandler.InitException;
+
 public class Charger {
-	
+		
+	// Attribute
 	int id;
 	boolean occupied;
+	int chargingStationId;
+	Log logger;
 	
+	// Constructor
+	Charger(int id, int chargingStationId) throws InitException{
+		/*
+		 * input : id of the charger, id of charger station that this charger belong to
+		 */
+		try {
+			logger = new Log("charger"+String.valueOf(this.id),"Charger "+ String.valueOf(this.id));
+			logger.init();
+			this.id = id;
+			this.chargingStationId = chargingStationId;
+		}catch(InitException e) {
+			System.out.print("Unable to initialize Charger "+ String.valueOf(id));
+		}
+		
+		
+	}
+	
+	// Functionality
+	void start_charge() {
+		this.logger.log.info("Charger " + String.valueOf(id) + "Start charging");
+	}
+	
+	void stop_charge() {
+		this.logger.log.info("Charger " + String.valueOf(id) + "Stop charging");
+		// set the occupied to false
+		this.occupied = false;
+	}
+	
+	
+	// setters and getter
 	public int getId() {
 		return id;
 	}
@@ -18,6 +53,7 @@ public class Charger {
 		this.occupied = occupied;
 	}
 	
+	// to strings
 	@Override
 	public String toString() {
 		return "Charger [id=" + id + ", occupied=" + occupied + "]";
---
+++ capstone_project/src/charging_station/ChargingStation.java
@@ -0,0 +1,112 @@
+package charging_station;
+
+import java.util.ArrayList;
+import charging_station.ExceptionHandler.InitException;
+import charging_station.ExceptionHandler.assigningException;
+import charging_station.ExceptionHandler.listeningException;
+
+// exceptions
+
+public class ChargingStation {
+	
+	// Attributes	
+	int id; // charging station id
+	ArrayList<Charger> chargers = new ArrayList<Charger>(); // list of charger in the charging station
+	String bookFilePath; // list of booking
+	Log logger;
+	
+	// Constructor
+	ChargingStation(int id){
+		/*
+		 * input : id of the charging station
+		 */
+		try {
+			logger = new Log("charger"+String.valueOf(this.id),"Charger "+ String.valueOf(this.id));
+			logger.init();
+			this.id = id;
+		}catch(InitException e) {
+			System.out.print("Unable to initialize Charging Station "+ String.valueOf(id));
+		}
+		
+	}
+
+
+	// Functionalities
+	String listening( String bookFilePath) throws listeningException{
+		/* 
+		 * read the booking file and return the information of waiting vehicle
+		 */
+			String vehicleInfo = null;
+			// read booking file
+			
+			// get the vehicle information if available (the booking time >= current time))	
+			
+			// return car information
+			if (vehicleInfo != null) {
+				this.logger.log.info("Handling a vehicle - " + vehicleInfo);
+			}
+			
+			return vehicleInfo;
+	}
+	
+	int availability(int vehId) {
+		/* 
+		 * return  id of free charger
+		 */
+		int idOfFreeCharger = -1;
+		
+		// check for available charger
+		this.logger.log.info("Finding available charger for vehicle " + String.valueOf(vehId));
+		for (Charger c:chargers) {
+			if (!c.occupied) {
+				this.logger.log.info("Charger " + String.valueOf(idOfFreeCharger) + " is available.");
+				idOfFreeCharger = c.id;
+				this.logger.log.info("Selecting charger " + String.valueOf(idOfFreeCharger));
+			}
+		}
+		
+		// return available charger		
+		return idOfFreeCharger;
+	}
+	
+	
+	void assigning(int vehId, int chargerId) throws assigningException {
+		this.logger.log.info("Assigning vehicle " + String.valueOf(vehId) + " to charger " + String.valueOf(chargerId));
+		// set the charger to occupied
+		for (Charger c:chargers) {
+			if (c.id == chargerId) {
+				c.occupied = true;
+			}
+		}	
+		
+				
+	}
+	
+	void book(int id, double timeSlot) {
+		/* 
+		 * book a time slot by any car to charge
+		 */
+		
+		// Logging
+		
+	}
+	
+
+	
+	
+	// setters and getters
+	public String getBookFilePath() {
+		return bookFilePath;
+	}
+
+	public void setBookFilePath(String bookFilePath) {
+		this.bookFilePath = bookFilePath;
+	}
+	
+	// to strings
+	@Override
+	public String toString() {
+		return "Charging Station [id=" + id +"]";
+	}
+
+}
